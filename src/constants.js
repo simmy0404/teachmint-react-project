ac4fb74f43a50d2f10e59362de436fe0f6c3b092
simmy-ts <simmy.gupta@tabsquare.com>
2023-12-12 12:18:00 +0530

    minor changes

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const USER_API = "https://jsonplaceholder.typicode.com/users";
 export const POSTS_API = "https://jsonplaceholder.typicode.com/posts";
-export const COUNTRY_API = "http://worldtimeapi.org/api/timezone";
+export const COUNTRY_API = "https://worldtimeapi.org/api/timezone";
